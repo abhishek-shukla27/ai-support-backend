00ca77ce0d5e390ec1dce4c59be8b9f14a727ac2
unknown <abhishekshukla2711@gmail.com>
2026-01-28 23:07:31 +0530

    issues fixed

--- app/main.py
+++ app/main.py
@@ -12,7 +12,10 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://abhishek-shukla27.github.io/ai-chat-demo/",
+                   "http://localhost:5500",
+                   "http://127.0.0.1:5500",
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
